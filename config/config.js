62fd49f5bb539548b3bbe327ffd935bcaf47b716
Hemant <hmewada74@gmail.com>
2024-04-08 14:05:02 +0530

    cors error

--- config/config.js
+++ config/config.js
@@ -5,5 +5,6 @@ const config = {
   PORT: String(process.env.PORT),
   MONGO_URL: String(process.env.MONGO_URL),
   JWT_SECRET: String(process.env.JWT_SECRET),
+  FRONTEND_URI: String(process.env.FRONTEND_URI),
 };
 module.exports = config;
--- socket/socket.js
+++ socket/socket.js
@@ -1,6 +1,7 @@
 const { Server } = require("socket.io");
 const http = require("http");
 const express = require("express");
+const config = require("../config/config");
 
 // create a server
 const app = express();
@@ -8,9 +9,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: ["http://localhost:3000"],
-    // methods: ["GET", "POST"],
-    origin: "*",
+    origin: [config.FRONTEND_URI],
+    methods: ["GET", "POST"],
+    // origin: "*",
     // methods: ["GET", "POST"],
   },
 });
